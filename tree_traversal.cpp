bfbd9ca6267cfe5c6c9fd971d2fb37a4fed833ad
Vaghela Hiren <u19ec145@eced.svnit.ac.in>
2021-06-05 12:33:35 +0530

    Tree Traversal

---
+++ tree_traversal.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int key;
+    Node *left;
+    Node *right;
+    Node (int x)
+    {
+        key=x;
+        right=left=NULL;
+    }
+};
+
+void bftraverse(Node *root)
+{
+    cout<<root->key<<endl;
+    while(root!=NULL)
+    {
+        cout<<root->left->key<<endl;
+        cout<<root->right->key<<endl;
+        root=root->left;
+    }
+}
+void dftraverse(Node *root)
+{
+    if(root!=NULL)
+    {
+        dftraverse(root->left);
+        cout<<root->key<<endl;
+        dftraverse(root->right);
+    }
+}
+
+int main()
+{
+    Node *root=new Node(10);
+    root->left=new Node(20);
+    root->right=new Node(30);
+    root->left->left= new Node(40);
+    root->left->right=new Node(50);
+    root->right->left=new Node(60);
+    root->right->right=new Node(70);
+    dftraverse(root);
+    return 0;
+}
