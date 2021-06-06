b7b608cf293f19f55ebd7f6bb3d649de995ce7d5
Vaghela Hiren <u19ec145@eced.svnit.ac.in>
2021-06-06 13:25:21 +0530

    left_view of binary tree

---
+++ tree/left_view.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int key;
+    Node *left;
+    Node *right;
+    Node (int x)
+    {
+        key=x;
+        right=left=NULL;
+    }
+};
+
+void left_view(Node *root)
+{
+    if(root==NULL)
+    return;
+    queue<Node *>q;
+    q.push(root);
+    while(!q.empty())
+    {
+        int count=q.size();
+        for(int i=0;i<count;i++)
+        {
+            Node *curr=q.front();
+            q.pop();
+            if(i==0)
+            cout<<curr->key<<endl;
+            if(curr->left!=NULL)
+            q.push(curr->left);
+            if(curr->right!=NULL)
+            q.push(curr->right);
+        }
+    }
+
+}
+
+
+int main()
+{
+    Node *root=new Node(10);
+    root->left=new Node(20);
+    root->right=new Node(30);
+    root->left->left= new Node(40);
+    root->left->right=new Node(50);
+    root->right->left=new Node(60);
+    root->right->right=new Node(70);
+    left_view(root);
+    return 0;
+}
